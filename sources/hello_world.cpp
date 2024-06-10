2ab1b70cdffa75ebf539b4dbb850bbe81f020550
Vovchansky1 <vsdor284@gmail.com>
2024-06-10 04:45:57 -0400

    Patch 2

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
-
-int main() {
-     std::string name;
-     std::cin >> name; // Checking updates
-     std::cout << "Hello world from " << name << std::endl;
-     return 0;
+int
+main()
+{
+  std::string name;
+  std::cin >> name; // Patch 2
+  std::cout << "Hello World from" << name << std::endl;
+  return 0;
 }
